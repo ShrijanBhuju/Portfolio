219bb4ff46de8380a2f9fd2ec495b299668be3c1
ShrijanBhuju <bhujushrijan@gmail.com>
2024-09-08 01:15:13 +0545

    Projects Section Completed

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// -----------------About-----------
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab")
+}
